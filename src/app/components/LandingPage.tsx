056efa67e07bdac3c606c65b54f3c9e15478f46e
gollavinaykumar <gollavinay13@gmail.com>
2024-12-20 14:55:26 +0530

    two pages added

---
+++ src/app/components/LandingPage.tsx
@@ -0,0 +1,120 @@
+"use client";
+import Image from "next/image";
+import "../page.module.css";
+import { Col, Divider, List, Rate, Row } from "antd";
+import happy from "../../../public/happy.jpg";
+import Title from "antd/es/typography/Title";
+
+export default function LandingPage() {
+  return (
+    <Row
+      style={{
+        width: "100%",
+        margin: 0,
+      }}
+      gutter={[16, 16]}
+    >
+      <Col xs={0} sm={2}></Col>
+
+      <Col
+        xs={24}
+        sm={10}
+        style={{
+          width: "100%",
+          padding: "20px",
+        }}
+      >
+        <div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
+          <Title style={{ textAlign: "justify", fontWeight: 600 }}>
+            Why We're Your Top Choice for Loads Transport
+          </Title>
+
+          <div>
+            <List>
+              <Title level={2}>Experienced Professionals</Title>
+              <List.Item>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Possimus pariatur iure tempora delectus blanditiis esse vel.
+                Eligendi enim esse dolorem magni facilis sed, culpa facere
+                dolorum aut, odit eveniet corrupti placeat, aliquid nisi minus
+                sint ducimus.
+              </List.Item>
+              <Divider variant="solid" style={{ borderColor: "black" }} />
+            </List>
+
+            <List>
+              <Title level={2}>Tailored Solutions</Title>
+              <List.Item>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Possimus pariatur iure tempora delectus blanditiis esse vel.
+                Eligendi enim esse dolorem magni facilis sed, culpa facere
+                dolorum aut, odit eveniet corrupti placeat, aliquid nisi minus
+                sint ducimus.
+              </List.Item>
+              <Divider variant="solid" style={{ borderColor: "black" }} />
+            </List>
+
+            <List>
+              <Title level={2}>Efficiency and Timeliness</Title>
+              <List.Item>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Possimus pariatur iure tempora delectus blanditiis esse vel.
+                Eligendi enim esse dolorem magni facilis sed, culpa facere
+                dolorum aut, odit eveniet corrupti placeat, aliquid nisi minus
+                sint ducimus.
+              </List.Item>
+            </List>
+          </div>
+        </div>
+      </Col>
+
+      <Col xs={0} sm={2}></Col>
+      <Col
+        xs={24}
+        sm={7}
+        style={{
+          width: "100%",
+          padding: "20px",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "20px",
+          }}
+        >
+          <Image
+            src={happy}
+            alt="Happy person"
+            style={{
+              borderRadius: "30px",
+              width: "100%",
+              height: "auto",
+            }}
+          />
+
+          <div
+            style={{
+              height: "200px",
+              backgroundColor: "black",
+              borderRadius: "30px",
+              color: "white",
+              padding: "20px",
+              textAlign: "center",
+            }}
+          >
+            <Rate value={5} />
+            <Title level={3} style={{ color: "white" }}>
+              4.8% From customers
+            </Title>
+            <Title level={5} style={{ color: "white" }}>
+              Lorem morbi et amet suscipit
+            </Title>
+          </div>
+        </div>
+      </Col>
+      <Col xs={0} sm={2}></Col>
+    </Row>
+  );
+}
---
+++ src/app/components/Lp3.tsx
@@ -0,0 +1,92 @@
+import { Col, Row, Typography } from "antd";
+import Image from "next/image";
+import lorry from "../../../public/Lorry.jpg";
+import Title from "antd/es/typography/Title";
+
+export default function Lp3() {
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "auto",
+        backgroundColor: "black",
+        padding: "50px 0",
+      }}
+    >
+      <Row
+        gutter={[16, 16]}
+        justify="center"
+        align="middle"
+        
+      >
+        <Col xs={24} sm={20} md={10} lg={8}>
+          <Image
+            src={lorry}
+            alt="lorry"
+            style={{
+              width: "100%",
+              height: "auto",
+              borderRadius: 30,
+            }}
+          />
+        </Col>
+
+        <Col xs={24} sm={20} md={12} lg={10}>
+          <div>
+            <Title level={2} style={{ color: "white" }}>
+              <span style={{ color: "yellow" }}>About Us:</span> Your Reliable
+            </Title>
+            <Title level={2} style={{ color: "white" }}>
+              Goods Moving Partner
+            </Title>
+            <Typography.Paragraph
+              style={{
+                color: "white",
+                textAlign: "justify",
+                lineHeight: "1.6",
+                marginBottom: "20px",
+              }}
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+              earum esse tenetur fugiat quam cumque consequuntur provident
+              laudantium? Distinctio, accusantium. Ullam sint similique modi vel
+              iure temporibus cumque ea cum, porro architecto illum ab odit quos
+              nisi soluta reiciendis dignissimos fuga expedita necessitatibus
+              dolore atque. Veritatis deserunt excepturi eligendi doloribus
+              molestiae porro aperiam. Laudantium, molestias nemo consequatur
+              cupiditate harum ea! Culpa aperiam fuga cum vel dicta impedit, est
+              placeat voluptate modi! Animi quasi sapiente consectetur quo ut
+              alias accusantium itaque.
+            </Typography.Paragraph>
+            <Row gutter={[16, 16]} justify="start">
+              <Col xs={8}>
+                <Title level={3} style={{ color: "yellow", marginBottom: 0 }}>
+                  32 +
+                </Title>
+                <Typography.Text style={{ color: "white" }}>
+                  Years Of Experience
+                </Typography.Text>
+              </Col>
+              <Col xs={8}>
+                <Title level={3} style={{ color: "yellow", marginBottom: 0 }}>
+                  12k
+                </Title>
+                <Typography.Text style={{ color: "white" }}>
+                  Satisfied Clients
+                </Typography.Text>
+              </Col>
+              <Col xs={8}>
+                <Title level={3} style={{ color: "yellow", marginBottom: 0 }}>
+                  20 +
+                </Title>
+                <Typography.Text style={{ color: "white" }}>
+                  Countries Served
+                </Typography.Text>
+              </Col>
+            </Row>
+          </div>
+        </Col>
+      </Row>
+    </div>
+  );
+}
