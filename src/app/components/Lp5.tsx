d438b15ef7c59bc521aba9e863fec9de52ee372f
gollavinaykumar <gollavinay13@gmail.com>
2024-12-21 00:36:52 +0530

    In Landing page 5th page added

---
+++ src/app/components/Lp5.tsx
@@ -0,0 +1,46 @@
+import { Button, Col, Row } from "antd";
+import Title from "antd/es/typography/Title";
+import { url } from "inspector";
+
+export default function Lp5() {
+  return (
+    <div
+      style={{
+        width: "100%",
+        backgroundImage: `url('../../traffics.avif')`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        padding: 50,
+      }}
+      className="banner-main"
+    >
+      <Row className="banner">
+        <Col span={2}></Col>
+        <Col span={20}>
+          <Title style={{ color: "yellow" }}>Mega Savings On Loads</Title>
+          <Title level={2} style={{ color: "white" }}>
+            {" "}
+            Streamline Your Move, Save Big, and Enjoy
+          </Title>
+          <Title level={2} style={{ color: "white" }}>
+            {" "}
+            a Stress-Free Relocation!
+          </Title>
+          <Button
+            style={{
+              backgroundColor: "yellow",
+              paddingLeft: 50,
+              paddingRight: 50,
+              paddingTop: 25,
+              paddingBottom: 25,
+              borderRadius: 10,
+            }}
+          >
+            Get Started
+          </Button>
+        </Col>
+        <Col span={2}></Col>
+      </Row>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import Lp2 from "./components/Lp2";
 import Lp3 from "./components/Lp3";
 import Lp4 from "./components/Lp4";
+import Lp5 from "./components/Lp5";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Lp2 />
       <Lp3 />
       <Lp4 />
+      <Lp5 />
     </div>
   );
 }
