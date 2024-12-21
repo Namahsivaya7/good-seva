333173d010cbbb9c7c47c713b16a4b762abf50db
gollavinaykumar <gollavinay13@gmail.com>
2024-12-21 22:14:12 +0530

    changes made for perfection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import FooterLp from "./components/FooterLp";
-import Lp2 from "./components/Lp2";
-import Lp3 from "./components/Lp3";
-import Lp4 from "./components/Lp4";
-import Lp5 from "./components/Lp5";
-import Lp6 from "./components/Lp6";
+import FooterLp from "./components/LandingPage/FooterLp";
+import Lp2 from "./components/LandingPage/Lp2";
+import Lp3 from "./components/LandingPage/Lp3";
+import Lp4 from "./components/LandingPage/Lp4";
+import Lp5 from "./components/LandingPage/Lp5";
+import Lp6 from "./components/LandingPage/Lp6";
 
 export default function Home() {
   return (
