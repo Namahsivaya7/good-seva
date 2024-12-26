8b7b07d030feafc64189237f5f054fa00dc2b698
gollavinaykumar <gollavinay13@gmail.com>
2024-12-26 22:30:22 +0530

    dashboard and navbar pages added

---
+++ src/app/Dashboard/Loads/page.tsx
@@ -0,0 +1,7 @@
+export default function Loads() {
+  return (
+    <div>
+      <h1>Loads</h1>
+    </div>
+  );
+}
---
+++ src/app/Dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { Space } from "antd";
+import Title from "antd/es/typography/Title";
+
+export default function Dashboard() {
+  return (
+    <Space>
+      <Title>Dashboard</Title>
+    </Space>
+  );
+}
